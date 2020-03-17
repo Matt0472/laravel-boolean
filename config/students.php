7b16179b5721e297390b5c49671a5675fca25087
Mattia Pedone <mattiapedone93@gmail.com>
2020-03-17 18:12:21 +0100

    up

--- config/students.php
+++ config/students.php
@@ -13,13 +13,13 @@
         ],
 
         [
-          'img' => 'https://www.boolean.careers/images/students/biagini.png',
-          'name' => 'Alessandro Biagini',
-          'eta' => '25 anni',
-          'azienda' => 'DISC SPA',
-          'ruolo' => 'web developer',
-          'descrizione' => 'Da giocatore professionista di basket a sviluppatore web. 6 mesi di impegno da MVP e un memorabile tap-in targato Boolean hanno garantito ad Alessandro un solido futuro come web developer.',
-          'genere' => 'm'
+          'img' => 'https://www.boolean.careers/images/students/poggini.png',
+          'name' => 'Paola Poggini',
+          'eta' => '24 anni',
+          'azienda' => 'Prima Assicurazioni',
+          'ruolo' => 'junior software engineer',
+          'descrizione' => 'A 24 anni, dopo aver conseguito il diploma linguistico ha deciso di intraprendere fin da subito un percorso nel mondo Tech. Ad oggi ricopre il ruolo di Junior Software Engineer.',
+          'genere' => 'f'
         ],
 
         [
--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -1,4 +1,19 @@
-@foreach (config('students.allStudents') as $student)
-    {{$student['name']}}
-@endforeach
+@extends('layouts.layout')
+@section('main-content')
+  @foreach ($students as $student)
+    <div class="student">
+      <div class="info">
+        <img src="{{$student['img']}}" alt="{{$student['name']}}">
+          <h2>
+            {{$student['name']}}({{$student['eta']}})
+          </h2>
+          <h3>
+            Assunt{{($student['genere'] == 'm') ? 'o' : 'a'}} da {{$student['azienda']}} 
+            come {{$student['ruolo']}}
+          </h3>
+      </div>
+      <p>{{$student['descrizione']}}</p>
+    </div>
+  @endforeach
+@endsection
 
