1a6bfa4f588741eb5740c105b77853a98d4aa4f3
Mattia Pedone <mattiapedone93@gmail.com>
2020-03-17 16:30:01 +0100

    up

---
+++ resources/views/layouts/partials/_footer.blade.php
@@ -0,0 +1,15 @@
+
+      <nav>
+        <ul>
+          <li>
+            <a href="{{route('static_page.faq')}}">Domande Frequenti</a>
+          </li>
+          <li>
+            <a href="{{route('static_page.privacy')}}">Privacy</a>
+          </li>
+        </ul>
+      </nav>
+    
+    </div>
+  </body>
+</html>
---
+++ resources/views/layouts/partials/_header.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  {{-- link al nostro stile --}}
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+  {{-- link google font --}}
+  <link href="https://fonts.googleapis.com/css?family=Rubik:400,700&display=swap" rel="stylesheet">
+
+  <title>Laravel Boolean Careers</title>
+</head>
+<body>
+  <div class="nav-container">
+    <ul>
+      <li>
+      <a href="{{route('static_page.home')}}">Home</a>
+      </li>
+      <li>
+        <a href="">Corso</a>
+      </li>
+      <li>
+        <a href="">Dopo il Corso</a>
+      </li>
+      <li>
+        <a href="">Lezione Gratuita</a>
+      </li>
+      <li>
+        <a href="">Candidati ora</a>
+      </li>
+    </ul>
+  </div>
+    <div class="wrapper">
