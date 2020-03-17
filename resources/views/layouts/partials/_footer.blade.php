79922cd0c7b19f90929f9bf5dc1fb1a330af752b
Mattia Pedone <mattiapedone93@gmail.com>
2020-03-17 19:12:50 +0100

    up

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -1,15 +1,36 @@
 
-      <nav>
-        <ul>
-          <li>
-            <a href="{{route('static_page.faq')}}">Domande Frequenti</a>
-          </li>
-          <li>
-            <a href="{{route('static_page.privacy')}}">Privacy</a>
-          </li>
-        </ul>
+  </div>
+      <nav class="nav-footer">
+        <div class="container-fluid-top">
+          <div class="nav-footer-left">
+            <img src="https://www.boolean.careers/images/common/logo-white.png" alt="">
+            <small>Sede Operativa</small>
+            <small>Via Carducci 12 - 20123 Milano</small>
+            <small>Tel: 02-36599712</small>
+          </div>
+          <div class="nav-footer-right">
+            <ul>
+              <li>
+                <a href="">Lezione Gratuita</a>
+              </li>
+              <li>
+                <a href="{{route('static_page.faq')}}">Domande Frequenti</a>
+              </li>
+              <li>
+                <a href="">Per le aziende</a>
+              </li>
+              <li>
+                <a href="{{route('static_page.privacy')}}">Privacy Policy</a>
+              </li>
+              <li>
+                <a href="">Lavora con noi</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="container-fluid-bottom">
+
+        </div>
       </nav>
-    
-    </div>
   </body>
 </html>
