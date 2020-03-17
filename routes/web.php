90727ce4794b0aee0980a47150250a3b7558e272
Mattia Pedone <mattiapedone93@gmail.com>
2020-03-17 15:32:54 +0100

    up:aggiunte le rotte marittime!!!!

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// creiamo una rotta, che ha come controller uno Static page controller
+Route::get('/', 'StaticPageController@index')->name('static_page.home');
+Route::get('/privacy', 'StaticPageController@privacy')->name('static_page.privacy');
+Route::get('/domande-frequenti', 'StaticPageController@faq')->name('static_page.faq');
+
+
+// creaimo una rotta con il controller per gli studenti
+Route::get('/students', 'StudentController@index')->name('student.index');
+Route::get('/students/show{id}', 'StudentController@show')->name('student.show');
