953742d9e363f97468236a42dbe0ef0e8f5987f3
Mattia Pedone <mattiapedone93@gmail.com>
2020-03-18 18:33:39 +0100

    up: show page

---
+++ resources/views/students/show.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.layout')
+@section('main-content')
+<section class="all-students">
+    <div class="student">
+      <div class="info">
+        <img src="{{$student['img']}}" alt="{{$student['name']}}">
+          <a href="{{route('student.show', ['slug' => $student['slug']])}}">
+            <h3>
+              {{$student['name']}}({{$student['eta']}})
+            </h3>
+          </a>
+          <span>
+            Assunt{{($student['genere'] == 'm') ? 'o' : 'a'}} da {{$student['azienda']}} 
+            come {{$student['ruolo']}}
+          </span>
+      </div>
+      <p>{{$student['descrizione']}}</p>
+      <a href="" class="linkedin-icon">
+        <i class="fab fa-linkedin"></i>
+      </a>
+    </div>
+    <a href="{{route('student.index')}}"><-Torna indietro</a>
+</section>
+@endsection
